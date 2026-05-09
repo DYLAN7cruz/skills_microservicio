d455d9d7b0709722d8a29f7fc9c5f9f9c1c8ed02
DYLAN7cruz <cruzdylan.2004@gmail.com>
2026-05-08 22:23:17 -0500

    inicio del proyecto con springboot

---
+++ src/main/java/com/dylan/cruz/microservicio_skills/MicroservicioSkillsApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.cruz.microservicio_skills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioSkillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioSkillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dylan/cruz/microservicio_skills/MicroservicioSkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.cruz.microservicio_skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
